9221b046658dbea21b83e9b81c1d5ba09ca4e98e
ayubsd <ayub1414000@gmail.com>
2025-05-07 23:49:45 +0530

    Update Practical 2 concat.pl

--- Practical 2 concat.pl
+++ Practical 2 concat.pl
@@ -1,2 +1,11 @@
 conc([],L,L).
 conc([H|T],L,[H|R]):- conc(T,L,R).
+
+
+
+# Queries : 
+?- conc([a,b], [c,d], X).          % X = [a, b, c, d]
+?- conc([], [1,2,3], X).           % X = [1, 2, 3]
+?- conc([1,2,3], [], X).           % X = [1, 2, 3]
+?- conc([x], [y,z], X).            % X = [x, y, z]
+?- conc(X, Y, [1,2]).              % Finds all ways to split [1,2] into two lists
