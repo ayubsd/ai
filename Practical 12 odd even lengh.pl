63d482fd84cfbb868dc5ae8d559a3d78561226d6
ayubsd <ayub1414000@gmail.com>
2025-05-08 00:04:28 +0530

    Update Practical 12 odd even lengh.pl

--- Practical 12 odd even lengh.pl
+++ Practical 12 odd even lengh.pl
@@ -1,7 +1,13 @@
-evenLength([]).
-evenLength([_|[_|List]]):-
-evenLength(List).
+evenLength([]).                     % Base case: an empty list has an even length (0).
+evenLength([_|[_|List]]) :-         % Recursive case: skips two elements and continues with the rest of the list.
+    evenLength(List).
 
-oddLength([_]).
-oddLength([_|[_|List]]):-
-oddLength(List).
+oddLength([_]).                      % Base case: a list with one element has an odd length.
+oddLength([_|[_|List]]) :-          % Recursive case: skips two elements and continues with the rest of the list.
+    oddLength(List).
+
+# Queries
+?- evenLength([1, 2, 3, 4]).   % true
+?- evenLength([1, 2, 3]).       % false
+?- oddLength([1, 2, 3]).        % true
+?- oddLength([1, 2, 3, 4]).     % false
