2a7a6bb2dc3204deb888d658a4d22ec743f402e2
ayubsd <ayub1414000@gmail.com>
2025-05-08 00:02:35 +0530

    Update Practical 10 member.pl

--- Practical 10 member.pl
+++ Practical 10 member.pl
@@ -1,2 +1,8 @@
 memb(X,[X|_]).
 memb(X,[_|T]):- memb(X,T).
+
+
+# Queries
+?- memb(3, [1, 2, 3, 4]).     % true
+?- memb(a, [b, c, d]).        % false
+?- memb(X, [1, 2, 3]).        % X = 1 ; X = 2 ; X = 3
