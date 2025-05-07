00e827790cf4dce6dd93fcea60ef97c5c8a6b0fc
ayubsd <ayub1414000@gmail.com>
2025-05-07 23:56:39 +0530

    Update Practical 5 max min.pl

--- Practical 5 max min.pl
+++ Practical 5 max min.pl
@@ -1,2 +1,10 @@
-max(X,Y,M) :- X>Y, M=X.
-max(X,Y,M) :- X<Y, M=Y.
+max(X, Y, M) :- X > Y, M = X.
+max(X, Y, M) :- X < Y, M = Y.
+max(X, Y, M) :- X =:= Y, M = X.   % Handles the case when X equals Y
+
+
+
+# Queries 
+?- max(5, 3, M).      % M = 5
+?- max(2, 7, M).      % M = 7
+?- max(4, 4, M).      % M = 4
