27a3843bf3273fbdfd8be937b3c028569563cc40
ayubsd <ayub1414000@gmail.com>
2025-05-07 23:53:00 +0530

    Update Practical 3.pl

--- Practical 3.pl
+++ Practical 3.pl
@@ -1,2 +1,16 @@
-conc([],L,L).
-conc([H|T],L,[H|R]):- conc(T,L,R).
+% reverse_list(InputList, ReversedList)
+reverse_list([], []).
+reverse_list([Head|Tail], Result) :-
+    reverse_list(Tail, ReversedTail),
+    append_list(ReversedTail, [Head], Result).
+
+% append_list(List1, List2, Result)
+append_list([], L, L).
+append_list([H|T], L, [H|R]) :-
+    append_list(T, L, R).
+
+
+# Queries:
+?- reverse_list([1,2,3], X).        % X = [3,2,1]
+?- reverse_list([a,b,c], Result).   % Result = [c,b,a]
+?- reverse_list([], R).             % R = []
