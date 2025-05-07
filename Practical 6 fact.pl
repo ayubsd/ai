57064e22e139c49b59b0096af49272f5bd401f8a
ayubsd <ayub1414000@gmail.com>
2025-05-07 23:57:58 +0530

    Update Practical 6 fact.pl

--- Practical 6 fact.pl
+++ Practical 6 fact.pl
@@ -1,4 +1,12 @@
-factorial(0,1).
-factorial(N,F):-
-N>0, N1 is N-1, factorial(N1,F1),
-F is N*F1.
+factorial(0, 1).  % Base case: 0! = 1
+factorial(N, F) :-
+    N > 0,
+    N1 is N - 1,
+    factorial(N1, F1),
+    F is N * F1.
+
+
+# Queries
+?- factorial(0, F).     % F = 1
+?- factorial(5, F).     % F = 120
+?- factorial(3, F).     % F = 6
