492c07cfdb084d2c0827c3ec57d1072222b5f3b9
ayubsd <ayub1414000@gmail.com>
2025-05-08 00:09:22 +0530

    Update Practical 13 maxlist.pl

--- Practical 13 maxlist.pl
+++ Practical 13 maxlist.pl
@@ -1,6 +1,10 @@
-maxList([M],M).
-maxList([H|T],M):-
+maxList([M], M).              
+maxList([H|T], M) :-         
+    maxList(T, M1),
+    (H > M1 -> M = H; M = M1).  
 
-maxList(T,M1),
 
-(H > M1 -> M=H; M=M1).
+# Queries
+?- maxList([3, 5, 2, 8, 1], Max).   
+?- maxList([4, 9, 12, 7], Max).    
+?- maxList([2, 2, 2], Max).         
