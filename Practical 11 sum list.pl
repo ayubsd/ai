c5d7c338926e08cdc286e6838e8f67e29bf376a2
ayubsd <ayub1414000@gmail.com>
2025-05-08 00:03:30 +0530

    Update Practical 11 sum list.pl

--- Practical 11 sum list.pl
+++ Practical 11 sum list.pl
@@ -1,3 +1,10 @@
-sumList([],0).
-sumList([H|T],S):-
-sumList(T,S1), S is H+S1.
+sumList([], 0).           % Base case: the sum of an empty list is 0
+sumList([H|T], S) :-     % Recursive case: sum the head and the sum of the tail
+    sumList(T, S1), 
+    S is H + S1.
+
+
+# Queries
+?- sumList([1, 2, 3, 4], S).    % S = 10
+?- sumList([10, 20, 30], S).    % S = 60
+?- sumList([], S).              % S = 0
